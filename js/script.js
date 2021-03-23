52c0f45cbb2b329b1f7102e2092f04b94e397637
Tommaso Borsatti <tommasoborsatti89@gmail.com>
2021-03-23 14:55:22 +0100

    MileStone 1 completa - inizio Milestone 2

--- js/script.js
+++ js/script.js
@@ -1,13 +1,9 @@
-// Milestone 1
-// Partendo dalla seguente struttura dati , mostriamo in pagina tutte le icone disponibili come da layout.
-// 
-// Milestone 2
-// Coloriamo le icone per tipo
-// 
+
+
 // Milestone 3
 // Creiamo una select con i tipi di icone e usiamola per filtrare le icone
 
-const icons = [
+const Icons = [
   {
     name: 'apple-alt',
     family: 'fas',
@@ -116,4 +112,49 @@ const icons = [
     prefix: 'fa-',
     category: "animal"
   },
+];
+
+
+// Milestone 1
+// Partendo dalla seguente struttura dati , mostriamo in pagina tutte le icone disponibili come da layout.
+
+// definisco il target dove andare a iniettare l html:
+const Target = $(".icons");
+
+// destrutturo:
+const {name, family, prefix, category} = Icons;
+
+// uso for each come un ciclo for per ciclare Icons e iniettare tutte le icone nel target:
+
+Icons.forEach((icon, index) => {
+
+  //scrivo l html da inserire con destrutturazione e literal template:
+
+  const Html =
+  `<div>
+    <i class="${icon.family} ${icon.prefix}${icon.name}"></i>
+    <div class="title">${icon.name}</div>
+  </div>`
+
+  Target.append(Html);
+});
+
+
+// Milestone 2
+// Coloriamo le icone per tipo
+
+// Provo il processo per associazione; definisco un array di elementi in cui associo categoria a colore.
+
+const Colors = [
+
+  { category: food,
+    color: #ff008d
+  },
+  { category: food,
+    color: 	#d14eea
+  },
+  { category: food,
+    color: #f96363
+  },
+
 ];
